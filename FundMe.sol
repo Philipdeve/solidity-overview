8db86d4972e474593eb5a845aa14b498880ffe98
PHILIPS IFEANYI <pio14philip@gmail.com>
2022-11-01 12:30:37 +0100

    new lessons learnt

--- FundMe.sol
+++ FundMe.sol
@@ -1,36 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe{
-    uint256 public minUsd = 1 * 1e18 ; //10 ** 18; 
+
+    using PriceConverter for uint256;
+
+    uint256 public minUsd = 5 * 1e18 ; // 1 * 10 ** 18; 
+    
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minUsd, "Insufficient Ether to fund");   
+        require(getConversionRate(msg.value) >= minUsd, "Insufficient Ether to fund");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;  
     }
 
-    function getPrice() public view returns(uint256){
-        //ABI
-        // 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (,int256 price,,,)  = priceFeed.latestRoundData(); // this will give ETH in terms of usd //1600.0000000
-        return uint256(price * 1e10); // 1**10 = 10000000000 //converting price to 18decimal point to match msg.value
-      
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+        funders = new address[](0);
 
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        // (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        // require(callSuccess, "Call failed");
     }
 
-    function getVersion() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
 
-    }
-
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountUsd;
-    }
-    
 }
---
+++ PriceConverter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter {
+        function getPrice() public view returns(uint256){
+        //ABI
+        // 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,)  = priceFeed.latestRoundData(); // this will give ETH in terms of usd //1600.0000000
+        return uint256(price * 1e10); // 1**10 = 10000000000 //converting price to 18decimal point to match msg.value
+      
+
+    }
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
+
+    }
+
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountUsd;
+    }
+}
